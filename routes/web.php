109c3be51e19be2a8b7f4b121e532c5882db3942
michaelrrrxxz <mangaoang.michaelangelo23@gmail.com>
2025-03-08 19:41:07 +0800

    Add Login Function

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+route::post('login',[AuthController::class, 'login'])->name('login');
